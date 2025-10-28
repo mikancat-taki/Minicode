2a3e8574f72fa969332a61ac736d09b5844adf20
mikancat10 <216382990+mikancat-taki@users.noreply.github.com>
2025-10-28 20:10:07 +0900

    Delete src directory

--- src/renderer/app.ts
+++
@@ -1,9 +0,0 @@
-import * as monaco from "monaco-editor";
-
-const container = document.getElementById("editor-container")!;
-const editor = monaco.editor.create(container, {
-  value: "// Hello — this is a Monaco editor instance\n",
-  language: "typescript",
-  theme: "vs-dark",
-  automaticLayout: true
-});
